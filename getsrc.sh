0f40cbc205f68e7d67501fe43dc17ae84b3eef57
Jari Aalto <jari.aalto@cante.net>
2012-01-30 15:17:31 +0200

    20071212-1

---
+++ getsrc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Original sources as of 2007-12-12
+
+wget http://www.netcordia.com/files/ttcp.1 \
+  http://www.netcordia.com/files/ttcp.c \
+  http://www.netcordia.com/files/ttcp.c-brl
